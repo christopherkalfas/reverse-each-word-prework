e516e9c98223f7e29283c35fef9482ac4f051256
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 21:49:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   array_1 = string.split(" ")
   array_2 = []
-  array_1.each do |string|
-    array_2 << string.reverse
+  array_1.each do |gnirts|
+    array_2 << gnirts.reverse
   end
   array_2.join(" ")
   
